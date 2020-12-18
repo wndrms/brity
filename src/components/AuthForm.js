c35800717b157f03edf63e8b55d70c911c091d0a
wndrms <wndrms2006@naver.com>
2020-12-18 17:03:55 +0900

    2020.12.18.17:00

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -17,8 +17,6 @@ const AuthForm = () => {
         }
     };
     const onSubmit = async(event) => {
-        console.log("email : ", email);
-        console.log("password : ", password);
         event.preventDefault();
         try {
             let data;
@@ -38,12 +36,15 @@ const AuthForm = () => {
             setError(err.message);
         }
     };
-    const toggleProceeding = () => setProceeding(true);
+    const toggleProceeding = () => setProceeding((prev) => !prev);
+    const headerstyle = Proceeding => ({
+        display: !Proceeding && "none"
+    });
     return(
         <>
             <div id="wrap">
-                <header id="header">
-                    <button><img src={process.env.PUBLIC_URL + '02-icon-01-outline-chevron-left.svg'} alt="이전"></img></button>
+                <header id="header" style={headerstyle(Proceeding)}>
+                    <button onClick={toggleProceeding}><img src={process.env.PUBLIC_URL + '02-icon-01-outline-chevron-left.svg'} alt="이전"></img></button>
                 </header>
                 <div id="content" className="content">
                     <div className="logo-wrap">
@@ -52,22 +53,28 @@ const AuthForm = () => {
                     </div>
                     { Proceeding ? (
                         <>
-                            <p>{email}</p>
+                            <p className="user-id">{email}</p>
                             <div className="form-box">
-                                <form action="">
-                                    <label for="password">비밀번호</label>
+                                <form>
+                                    <label for="login-pw">비밀번호</label>
                                     <input 
                                         name="password" 
                                         type="password" 
                                         className="input-basic" 
+                                        id="login-pw"
                                         placeholder="비밀번호를 입력하세요"
                                         value={password}
                                         onChange={onChange}/>
+                                        <button type="submit"></button>
+                                        <div className="message">{error}</div>
                                 </form>
+                                <div className="check-circle square">
+                                    <input type="checkbox" name="pw-remember" id="pw-re-cb"/>
+                                    <label for="pw-re-cb">비밀번호 저장</label>
+                                </div>
                                 <div className="btn-wrap">
                                     <a href="#">비밀번호를 잊으셨나요?</a>
                                     <button className="btn-basic next" onClick={onSubmit}>다음</button>
-                                    {error && <span className="authError">{error}</span>}
                                 </div>
                             </div>
                         </>
@@ -76,21 +83,24 @@ const AuthForm = () => {
                             <p>안녕하세요, 브리티 관리자 페이지 입니다😀<br></br>
                                 브리티와 함께 영역을 확장해볼까요?</p>
                             <div className="form-box">
-                                <form action="">
-                                    <label for="email">이메일</label>
+                                <form>
+                                    <label for="user-id">이메일</label>
                                     <input 
                                         name="email" 
                                         type="text" 
-                                        className="input-basic" 
+                                        className="input-basic"
+                                        id="user-id" 
                                         placeholder="이메일이나 아이디를 입력하세요"
                                         onChange={onChange}/>
+                                    <button type="submit"></button>
+                                    <div className="message">{error}</div>
                                 </form>
                                 <div className="btn-wrap">
                                     <a href="#">이메일을 잊으셨나요?</a>
                                     <button className="btn-basic next" onClick={toggleProceeding}>다음</button>
                                 </div>
                             </div>
-                            <a href="#" className="login-google">Continue with Google</a>         
+                            <a href="#" className="login-google btn-purple enable">Continue with Google</a>         
                         </>
                         )}
                     <Link to="/SignUp">
